2f4c87ade053bf493e7a854b944ff0ab219a195b
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-10-04 19:50:45 +0330

    1

--- src/components/product03.jsx
+++ src/components/product03.jsx
@@ -1,26 +1,21 @@
 import { Component } from "react";
 class Product extends Component {
   state = {};
-  count = 5;
+  count = 0;
 
   render() {
     return (
       <>
-        {this.count !== 0 ? (
-          <>
-            <span className="m-2 text-info">product name</span>
-            <span className="m-2 badge bg-primary">{this.count}</span>
-            <button className="m-2 btn btn-sm btn-success">+</button>
-            <button className="m-2 btn btn-sm btn-warning ">-</button>
-            <button className="m-2 btn btn-sm btn-danger">delete</button>
-          </>
-        ) : (
-          <>
-            <h1>there is no product</h1>
-          </>
-        )}
+        <span className="m-2 text-info">product name</span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
+        <button className="m-2 btn btn-sm btn-success">+</button>
+        <button className="m-2 btn btn-sm btn-warning ">-</button>
+        <button className="m-2 btn btn-sm btn-danger">delete</button>
       </>
     );
+  }
+  format(count) {
+    return this.count === 0 ? "zero" : this.count;
   }
 }
 
