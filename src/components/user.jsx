5daf744096d2761c44b8e7f37988fb00ae1ccaae
fdahmadi <ahmadi.fd@gmail.com>
2023-10-09 12:50:45 -0700

    1

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,5 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const User = () => {
-  return (<h1>User</h1>);
+  const routeParams = useParams();
+const [user,setuser]=useState({});
+let user1;
+  useEffect(() => {
+    
+      getapiresult();
+    
+  }, []);
+  return (<h1>User - id = {user.first_name}</h1>);
+
+  async function getapiresult() {
+    user1 = await axios.get(`https://reqres.in/api/users/${routeParams.id}`);
+    setuser(user1.data.data);
+console.log(user1);
+  }
 };
 
 export default User;
