0647233f65be3b04be6b797f00c6e647666e4c5e
ahmadifd <ahmadi.fd@gmail.com>
2023-10-20 20:47:05 +0330

    1

--- src/components/app01/Store.jsx
+++ src/components/app01/Store.jsx
@@ -9,11 +9,11 @@ const userSlice = createSlice({
   name: "user",
   initialState: { username: "" },
   reducers: {
-    login: (state, action) => {
+    login (state, action)  {
       // {username : "sarvin"}
       state.username = action.payload.username;
     },
-    logout: (state) => {
+    logout(state)  {
       state.username = "";
     },
   },
